0dfa2121bafba097d9bcca7d04bc98190389b71d
jmschu <jmschu@umich.edu>
2019-09-09 14:37:38 -0400

    Lab_1

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+#Your name
+#Two random numbers between 0-100
+#The sum of the two numbers, starting with "Sum = "
+#The average of the two numbers, starting with "Average = 
+from random import randint
+
+print("Joseph Schuman")
+a = randint(1,100)
+b = randint(1,100)
+print(a)
+print(b)
+print("Sum = ",str((a+b)))
+print("Average = ",str(((a + b)/ 2.0)))
